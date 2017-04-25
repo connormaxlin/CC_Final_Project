80b174105981311fb2806be60a8389e654f23be7
connormaxlin <connormaxlin@gmail.com>
2017-04-25 00:49:24 -0400

    Updates

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,9 +1,27 @@
+var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+var textHPos = 100;
+
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(window.innerWidth - 10, window.innerHeight - 10);
+    // instructions:
+    myRec.onResult = showResult;
+    myRec.start();
 }
 
 function draw() {
     textSize(56);
     textFont("Chaparral Pro");
-    text("The truth is...", 100, 100);
+    text("The truth is...", 100, textHPos);
+    textSize(18);
+    textFont("Arial");
+    text("(Say something)", 100, textHPos+40);
+}
+
+function showResult() {
+    if (myRec.resultValue == true) {
+        textSize(56);
+        textFont("Chaparral Pro");
+        text("The truth is " + myRec.resultString, 100, textHPos+=80);
+        console.log(myRec.resultString);
+    }
 }
